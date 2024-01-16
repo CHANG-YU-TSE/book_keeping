8b03ccff1ea906293d610faef3a2cccde89c1471
CHANG-YU-TSE <156166233+CHANG-YU-TSE@users.noreply.github.com>
2024-01-16 16:18:47 +0800

    Update getData_post.js

--- functions/getData_post.js
+++ functions/getData_post.js
@@ -17,11 +17,20 @@ exports.handler = async (event, context) => {
   // 從 POST 資料中獲取引數 
     const sqlStatement = JSON.parse(event.body).sql || 'No value provided for sql';
 
-    const host = JSON.parse(event.body).host ||'mysql.sqlpub.com';
-    const user = JSON.parse(event.body).user ||'herb_db_user';
-    const password = JSON.parse(event.body).password || 'nk45Mte4Zhb5hw9K';
-    const database = JSON.parse(event.body).database ||'herb_db';
-    const port = JSON.parse(event.body).port || 3306;
+    const host_p = JSON.parse(event.body).host ||'mysql.sqlpub.com';
+    const user_p = JSON.parse(event.body).user ||'herb_db_user';
+    const password_p = JSON.parse(event.body).password || 'nk45Mte4Zhb5hw9K';
+    const database_p = JSON.parse(event.body).database ||'herb_db';
+    const port_p = JSON.parse(event.body).port || 3306;
+
+
+      const dbConfig = {
+          host: host_p ,
+          user: user_p,
+          password: password_p,
+          database: database_p ,
+          port: port_p 
+     };
 
   
   // 開始連  DB  
