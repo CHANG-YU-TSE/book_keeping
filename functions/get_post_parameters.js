c5d9d9a869a2e9406411e54265056d85e18d8a4d
CHANG-YU-TSE <156166233+CHANG-YU-TSE@users.noreply.github.com>
2024-01-16 13:35:24 +0800

    Update get_post_parameters.js

--- functions/get_post_parameters.js
+++ functions/get_post_parameters.js
@@ -4,13 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 exports.handler = async (event, context) => {
-  
-
- 
- // 確認請求方法為 POST
+  // 確認請求方法為 POST
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
@@ -19,11 +14,14 @@ exports.handler = async (event, context) => {
   }
 
   // 從 POST 資料中獲取引數 abc
-  const abcValue = JSON.parse(event.body).sql || 'No value provided for sql';
+  const abcValue = JSON.parse(event.body).abc || 'No value provided for abc';
 
   // 回傳引數 abc 的值給呼叫者
   return {
     statusCode: 200,
-    body: `Value of sql: ${sqlValue}`,
+    body: `Value of abc: ${abcValue}`,
+  };
+};
+
   };
 };
